1549dbe2a56a0a641c0fe1d8b21adffc0d866354
Vaibhav0302 <vadeshmukh7744@gmail.com>
2023-02-26 11:56:39 +0530

    Complete Website

--- script.js
+++ script.js
@@ -12,16 +12,25 @@ const options = {
 
 const apiUrl = "https://api.api-ninjas.com/v1/jokes?limit=1";
 
-async function getJoke(){
+async function getJoke() {
 
-    jokeEl.innerText="Updating...";
-    btnEl.Disabled = true;
-    btnEl.innerText = "loading";
-    const response = await fetch(apiUrl, options);
-    const data = await response.json();
-    jokeEl.innerText = data[0].joke;
-    btnEl.Disabled = false;
-    btnEl.innerText = "Tell Me a Joke";
+
+    try {
+        jokeEl.innerText = "Updating...";
+        btnEl.Disabled = true;
+        btnEl.innerText = "loading";
+        const response = await fetch(apiUrl, options);
+        const data = await response.json();
+        jokeEl.innerText = data[0].joke;
+        btnEl.Disabled = false;
+        btnEl.innerText = "Tell Me a Joke";
+
+    } catch (error) {
+        jokeEl.innerText = "An error occured, Try again later! 😊";
+        btnEl.Disabled = false;
+        btnEl.innerText = "Tell Me a Joke"
+    }
+
 }
 
 btnEl.addEventListener("click", getJoke);
