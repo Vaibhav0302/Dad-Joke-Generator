ecf5cd136a611c576c84e0f854eb51ab72eb9862
Vaibhav0302 <vadeshmukh7744@gmail.com>
2023-02-26 11:43:26 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
 
+const apiKey = "BvuA/BSsAO/CBumFaVrEfQ==r5I6A3QFxXcFUfiI";
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+}
+
+const apiUrl = "https://api.api-ninjas.com/v1/jokes?limit=1";
+
+async function getJoke(){
+
+    jokeEl.innerText="Updating...";
+    btnEl.Disabled = true;
+    btnEl.innerText = "loading";
+    const response = await fetch(apiUrl, options);
+    const data = await response.json();
+    jokeEl.innerText = data[0].joke;
+    btnEl.Disabled = false;
+    btnEl.innerText = "Tell Me a Joke";
+}
+
+btnEl.addEventListener("click", getJoke);
